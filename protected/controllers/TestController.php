da3103efa867f9868c207713b5a1bbb2f63371ec
elicefengzhi <elicefengzhi@gmail.com>
2016-08-04 22:17:11 +0800

    jpush

--- protected/controllers/TestController.php
+++ protected/controllers/TestController.php
@@ -8,9 +8,10 @@ class TestController extends WebsiteController {
         $client = new \JPush('c15e1ac0a68a4ae48509992d', '6e84eaae55ac654eae2de204', $config['logPath']);
         $result = $client->push()
             ->setPlatform(array('ios', 'android'))
-            ->addAllAudience()
+            ->addAlias(md5('md_15618294696'))
+            //->addAllAudience('all')
             ->setNotificationAlert('Hi, JPush')
-            ->addAndroidNotification('Hi, android notification', 'notification title', 1, array('data' => '{"type":"app","url":"nil","title":"您的订单尚未上传小结","ad":{"classname":"com.shoushuzhitongche.app.view.order.OrderDetailsActivity","param":{"jpush":"jpush","key1":"http://mdapi.mingyizhudao.com/apimd/orderinfo/27540"}}}'))
+            ->addAndroidNotification('Hi, android notification', 'notification title', 1, array('data' => '{"iosVersion":[ "2.1.0" ], "adVersion":[ "2.1.2.0" ], "type":"app", "url":"https://m.baidu.com/", "title":"百度", "isNeedLogin":"0", "ad":{ "classname":"com.shoushuzhitongche.app.view.receive.ReceiveInviteActivity", "param":{ "jpush":"jpush", "parm_action_url":"http://mdapi.mingyizhudao.com/apimd/orderinfo/27540"}}}'))
             ->addIosNotification("Hi, iOS notification", 'iOS sound', '+1', true, 'iOS category', array('data' => '{"iosVersion":["2.1.1"],"type":"app","isNeedLogin":"1","url":"nil","title":"您的订单尚未上传小结","ios":{"classname":"OrderDetailsViewController","param":{"isSelectDoctor":"1","serActionUrl":"http://mdapi.mingyizhudao.com/apimd/orderinfo/1210"}}}'))
             ->setMessage("msg content", 'msg title', 'type', array("key1"=>"value1", "key2"=>"value2"))
             //->setOptions(100000, 3600, null, false)
