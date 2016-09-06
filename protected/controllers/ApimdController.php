c5a059928f0e28c94ff37a032bed618ae54602ec
elicefengzhi <elicefengzhi@gmail.com>
2016-09-06 11:40:23 +0800

    test

--- protected/controllers/ApimdController.php
+++ protected/controllers/ApimdController.php
@@ -141,7 +141,6 @@ class ApimdController extends Controller {
                 $output = $apiService->loadApiViewData();
                 break;
             case 'contractdoctorlist'://签约医生列表
-                echo 111;exit;
                 $apiService = new ApiViewDoctorSearch($values);
                 $output = $apiService->loadApiViewData();
                 break;
