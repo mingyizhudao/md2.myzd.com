35efea80ce4e3f4e4680acf5fa5f84c209db62ae
lk <kui.liu@mingyizhudao.com>
2016-09-08 19:03:48 +0800

    医生信息录入

--- protected/modules/doctor/models/newdoctor/IndustrialInfoForm.php
+++ protected/modules/doctor/models/newdoctor/IndustrialInfoForm.php
@@ -67,7 +67,7 @@ class IndustrialInfoForm extends EFormModel {
 
     public function loadOptionsSubcat() {
         if (is_null($this->options_subcat)) {
-            $this->options_subcat = CHtml::listData(DiseaseCategory::model()->loadAllCatSub(), 'cat_id', 'cat_name');
+            $this->options_subcat = CHtml::listData(DiseaseCategory::model()->loadAllCatSub(), 'sub_cat_id', 'sub_cat_name');
         }
         return $this->options_subcat;
     }
