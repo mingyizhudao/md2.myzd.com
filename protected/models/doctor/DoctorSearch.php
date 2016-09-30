b41d6dd6b51b8ec0c2b8c589119d9f5997298edd
elicefengzhi <elicefengzhi@gmail.com>
2016-09-30 15:32:31 +0800

    医生专业

--- protected/models/doctor/DoctorSearch.php
+++ protected/models/doctor/DoctorSearch.php
@@ -78,10 +78,10 @@ class DoctorSearch extends ESearchModel {
             }
             // disease_sub_category.
             if (isset($this->queryParams['disease_sub_category'])) {
-                $category_id = $this->queryParams['disease_sub_category'];
+                $sub_cat_id = $this->queryParams['disease_sub_category'];
                 $this->criteria->join = 'left join disease_doctor_join b on t.id=b.doctor_id left join category_disease_join c on c.disease_id=b.disease_id ';
                 $this->criteria->addCondition("c.sub_cat_id=:category_id");
-                $this->criteria->params[":category_id"] = $category_id;
+                $this->criteria->params[":category_id"] = $sub_cat_id;
                 $this->criteria->distinct = true;
 
 //                $category_id= $this->queryParams['disease_sub_category'];
@@ -90,6 +90,8 @@ class DoctorSearch extends ESearchModel {
 //                $this->criteria->params[":sub_cat_id"] = $category_id;
 ////                $this->criteria->order = 'expteam_id desc';
 //                $this->criteria->distinct = true;
+                //医生专业
+                $this->criteria->compare('t.sub_cat_id', $sub_cat_id);
             }
         }
     }
