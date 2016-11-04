0b1971c9f4bb29a609bc38735757b8946db54867
zhangpengcheng <zhangpch666@163.com>
2016-11-04 10:02:48 +0800

    Merge branch 'local' of https://github.com/mingyizhudao/md2.myzd.com into local
    # Conflicts:
    #	themes/md2/js/custom/card.js
    #	themes/md2/js/qiniu/js/idCardUpload.js
    #	themes/md2/js/qiniu/js/idCardUpload.min.js

--- protected/apiservices/ApiForPayment.php
+++ protected/apiservices/ApiForPayment.php
@@ -18,7 +18,66 @@ class ApiForPayment
         return self::$_instance;
     }
 
-    public function getRequest($data){
+    /**
+     * 子账户注册接口
+     * @param $user_id
+     */
+    public function registerAccount($user_id){
+        $arg = [
+            'bindmobile' => '',  //绑定手机号
+            'linkman' => '',     //联系人姓名
+            'idcard' => '',      //个人身份证号
+            'bankaccountnumber' => '', //银行卡号
+            'bankname' => '',   //银行卡开户行
+            'accountname' => '', //银行卡开户名
+            'bankprovince' => '', //银行开户省
+            'bankcity' => '', //银行卡开户市
+
+        ];
+        /**
+         * @var $user User
+         */
+        $user = User::model()->getById($user_id);
+        if($user) {
+            $arg['bindmobile'] = $user->getMobile();
+            $arg['linkman'] = $user->getUserDoctorProfile()->getName();
+        }
+    }
+
+    /**
+     * 账户信息修改接口
+     * @param $data
+     */
+    public function updateAccount($data) {
+        $arg = [
+            'bankaccountnumber' => '', //银行卡号
+            'bankname' => '', //开户行
+            'accountname' => '', //开户名
+            'bankprovince' => '', //开户省
+            'bankcity' => '', //开户市
+            'callbackurl' => '', //后台回调地址
+            'bindmobile' => '', //绑定手机号
+        ];
+    }
+
+    /**
+     * 转账接口
+     */
+    public function giroAccount() {
+
+    }
+
+    /**
+     * 转账查询接口
+     */
+    public function giroHistory() {
+
+    }
+
+    /**
+     * 账户余额接口
+     */
+    public function accountBalance() {
 
     }
 }
