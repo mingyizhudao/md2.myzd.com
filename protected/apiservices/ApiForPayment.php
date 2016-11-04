c7ec2c2a2b98b62612c3ce360637eb902601a75f
zhangpengcheng <zhangpch666@163.com>
2016-11-04 20:00:56 +0800

    账户

--- protected/apiservices/ApiForPayment.php
+++ protected/apiservices/ApiForPayment.php
@@ -10,6 +10,15 @@ class ApiForPayment
 {
     private static $_instance;
 
+    //注册url
+    private $register_url = '';
+    //激活url
+    private $activate_url = '';
+    //转账url
+    private $giro_url = '';
+    //资质文件url
+    private $file_url = 'http://file.mingyizhudao.com/api/loadrealauth?userId=';
+
     public static function instance() {
         if(self::$_instance == null) {
             self::$_instance = new self();
@@ -19,8 +28,108 @@ class ApiForPayment
     }
 
     /**
+     * Http post请求
+     * @param $url
+     * @param $data
+     * @return mixed
+     */
+    public function HttpPost($url, $data) {
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_BINARYTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+        $header = array("Content-Type:text/html;charset=UTF-8");
+
+        curl_setopt($curl, CURLOPT_HTTPHEADER,$header);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+        $result = $this->executeByTimes($curl, 1);
+        return $result;
+    }
+
+    public function HttpGet($url, $id) {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url.$id);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_BINARYTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        $result = $this->executeByTimes($curl, 1);
+        return $result;
+    }
+    /**
+     * 失败执行机制
+     * @param $curl
+     * @param $times
+     * @return mixed
+     */
+    public function executeByTimes($curl, $times) {
+        $result = curl_exec($curl);
+        $info = curl_getinfo($curl);
+        $code = $info["http_code"];
+
+        if (curl_errno($curl) != 0 && $code != 200) {
+            $times--;
+            if ($times > 0) {
+                $result = $this->executeByTimes($curl, $times);
+            }
+        }
+        curl_close($curl);
+
+        return $result;
+    }
+
+    /**
+     * @param $bank DoctorBankCard
+     * @return string
+     */
+    public function checkValid($bank) {
+        $error = [
+            '1002' => '联系人姓名未填写',
+            '1003' => '身份证号未填写',
+            '1004' => '银行卡号未填写',
+            '1005' => '银行卡开户行未填写',
+            '1006' => '银行卡开户名未填写',
+            '1007' => '银行卡开户省未填写',
+            '1008' => '银行开户市未填写',
+        ];
+        $ret_msg = '';
+        if($bank->identification_card == ''){
+            $ret_msg = $error['1003'];
+        }
+        if($bank->name == '') {
+            $ret_msg = $error['1002'];
+        }
+        if($bank->card_no == '') {
+            $ret_msg = $error['1004'];
+        }
+        if($bank->bank == '') {
+            $ret_msg = $error['1005'];
+        }
+        if($bank->state_name == '') {
+            $ret_msg = $error['1007'];
+        }
+        if($bank->city_name == '') {
+            $ret_msg = $error['1008'];
+        }
+
+        return $ret_msg;
+    }
+    /**
+     * 账户查询接口
+     * @param $user_id
+     */
+    public function accountCheck($user_id) {
+
+    }
+
+    /**
      * 子账户注册接口
      * @param $user_id
+     * @return array
      */
     public function registerAccount($user_id){
         $arg = [
@@ -34,14 +143,76 @@ class ApiForPayment
             'bankcity' => '', //银行卡开户市
 
         ];
+
         /**
          * @var $user User
          */
         $user = User::model()->getById($user_id);
         if($user) {
             $arg['bindmobile'] = $user->getMobile();
-            $arg['linkman'] = $user->getUserDoctorProfile()->getName();
+            $profile = $user->getUserDoctorProfile();
+            $username = '';
+            if($profile) {
+                $username = $profile->getName();
+            } else {
+                return ['code' => 1, 'msg' => '用户信息基本不完整', 'result' => []];
+            }
+
+            if($username == '') {
+                return ['code' => 1, 'msg' => '用户手机未填写', 'result' => []];
+            }
+
+            $arg['linkman'] = $username;
+            $bank = DoctorBankCard::model()->getByAttributes(['user_id' => $user_id, 'is_active' => 0]);
+            $msg = $this->checkValid($bank);
+            if($bank && $msg == '') {
+                $arg['idcard'] = $bank->identification_card;
+                $arg['bankaccountnumber'] = $bank->card_no;
+                $arg['bankname'] = $bank->bank;
+                $arg['accountname'] = $username;
+                $arg['bankprovince'] = $bank->state_name;
+                $arg['bankcity'] = $bank->city_name;
+            } else {
+                return ['code' => 2, 'msg' => '银行卡信息不完整', 'result' => []];
+            }
+        } else {
+            return ['code' => 1, 'msg' => '未找到用户', 'result' => []];
         }
+
+        $result = $this->HttpPost($this->register_url, $arg);
+        return ['code' => 0, 'msg' => '', 'result' => $result];
+    }
+
+    /**
+     * 激活账户
+     * @param $user_id
+     * @return array
+     */
+    public function activateAccount($user_id) {
+        $arg = [
+            'ledgerno' => '',
+            'file' => [],
+        ];
+        $file = [];
+        $file_info = $this->HttpGet($this->file_url, $user_id);
+        $result = json_decode($file_info);
+        if($result['status'] == 'ok' && isset($result['files']) && !empty($result['files'])) {
+            foreach($result['status'] as $item) {
+                $file[] = ['type' => $item['type'], 'url' => $item['absFileUrl']];
+            }
+        }
+        if(empty($file)) {
+            return ['code' => 1, 'msg' => '用户照片未上传', 'result' => []];
+        }
+        $arg['file'] = $file;
+        $bank = DoctorBankCard::model()->getByAttributes(['user_id' => $user_id, 'is_active' => 0]);
+        if($bank) {
+            $arg['ledgerno'] = $bank->ledgerno;
+        } else {
+            return ['code' => 2, 'msg' => '用户未添加银行卡', 'result' => []];
+        }
+        $result = $this->HttpPost($this->activate_url, $arg);
+        return ['code' => 0, 'msg' => '', 'result' => $result];
     }
 
     /**
@@ -64,8 +235,14 @@ class ApiForPayment
      * 转账接口
      */
     public function giroAccount() {
-
+        $file_info =
+        $arg = [
+            'ledgerno' => '', //子账户商户编号
+            'amount' => '', //转账金额
+        ];
+        $result = $this->HttpPost($this->activate_url, $arg);
     }
+
 
     /**
      * 转账查询接口
@@ -78,6 +255,6 @@ class ApiForPayment
      * 账户余额接口
      */
     public function accountBalance() {
-
+        $ledgerno = '';
     }
 }
--- protected/models/doctor/DoctorBankCard.php
+++ protected/models/doctor/DoctorBankCard.php
@@ -9,6 +9,7 @@
  * @property string $name
  * @property integer $card_no
  * @property string $card_type
+ * @property string $identification_card
  * @property integer $state_id
  * @property string $state_name
  * @property integer $city_id
@@ -16,6 +17,8 @@
  * @property string $bank
  * @property string $subbranch
  * @property integer $is_default
+ * @property string $ledgerno
+ * @property integer $is_active
  * @property string $date_updated
  * @property string $date_deleted
  * @property string $date_created
@@ -39,8 +42,9 @@ class DoctorBankCard extends EActiveRecord {
         // NOTE: you should only define rules for those attributes that
         // will receive user inputs.
         return array(
-            array('user_id, card_no, state_id, city_id, is_default', 'numerical', 'integerOnly' => true),
-            array('name, state_name, card_type, city_name, bank, subbranch', 'length', 'max' => 50),
+            array('user_id, card_no, state_id, city_id, is_default, is_active', 'numerical', 'integerOnly' => true),
+            array('name, state_name, card_type, city_name, bank, subbranch, ledgerno', 'length', 'max' => 50),
+            array('identification_card', 'length', 'max' => 20),
             array('date_updated, date_deleted, date_created', 'safe'),
             // The following rule is used by search().
             // @todo Please remove those attributes that should not be searched.
@@ -67,6 +71,7 @@ class DoctorBankCard extends EActiveRecord {
             'id' => 'ID',
             'user_id' => 'User',
             'name' => '持卡人姓名',
+            'identification_card' => '身份证号',
             'card_no' => '卡号',
             'card_type' => '卡类型',
             'state_id' => 'State',
@@ -76,6 +81,8 @@ class DoctorBankCard extends EActiveRecord {
             'bank' => '开户银行',
             'subbranch' => '银行支行',
             'is_default' => '默认0 不是 1 是',
+            'ledgerno' => '子商户编号',
+            'is_active' => '激活状态0:等待激活1:可以使用2:无效卡',
             'date_updated' => 'Date Updated',
             'date_deleted' => 'Date Deleted',
             'date_created' => 'Date Created',
