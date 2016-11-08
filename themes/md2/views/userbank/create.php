9be72cf9532b3705461b87d48f647febef2715f1
jiangyan <18556506973@163.com>
2016-11-08 14:23:52 +0800

    银行卡上传

--- themes/md2/views/userbank/create.php
+++ themes/md2/views/userbank/create.php
@@ -1,6 +1,7 @@
 <?php
 Yii::app()->clientScript->registerScriptFile('http://static.mingyizhudao.com/jquery.formvalidate.min.1.0.js', CClientScript::POS_END);
 // Yii::app()->clientScript->registerScriptFile('http://static.mingyizhudao.com/card.min.1.0.js', CClientScript::POS_END);
+// Yii::app()->clientScript->registerScriptFile(Yii::app()->theme->baseUrl . '/js/qiniu/js/bankCreate.js?ts=' . time(), CClientScript::POS_END);
 Yii::app()->clientScript->registerScriptFile(Yii::app()->theme->baseUrl . '/js/custom/card.js?ts=' . time(), CClientScript::POS_END);
 ?>
 <?php
@@ -10,14 +11,19 @@ $urlLoadCity = $this->createUrl('/region/loadCities', array('state' => ''));
 $urlAjaxCreate = $this->createUrl('userbank/ajaxCreate');
 $urlIdentify = $this->createUrl('userbank/identify', array('card_id' => ''));
 $urlCardList = $this->createUrl('userbank/cardList', array('addBackBtn' => 1));
+$isFirst = Yii::app()->request->getQuery('isFirst', '');
+$ajaxDoctorRealAuth = $this->createUrl('qiniu/ajaxDoctorRealAuth');
+$urlQiniuAjaxToken = $this->createUrl('qiniu/ajaxDrToken');
+// var_dump($user_id);die;
+$urlDoctorRealAuth = 'http://file.mingyizhudao.com/api/loadrealauth?userId=' . $user_id;
 $this->show_footer = false;
-// var_dump($model);die;
+// var_dump($isFirst);die;
 ?>
 <style>
     .w130p{width:130px;}
     .c-h{color:#A9A9A9;}
 </style>
-<article id="userbankCreate_article" class="active userbank_article" data-scroll="true">
+<article id="userbankCreate_article" class="active userbank_article" data-scroll="true"data-realAuth="<?php echo $urlDoctorRealAuth; ?>">
     <div class="pt10">
         <?php
         $form = $this->beginWidget('CActiveForm', array(
@@ -92,7 +98,7 @@ $this->show_footer = false;
                         echo $form->dropDownList($model, 'state_id', $model->loadOptionsState(), array(
                             'name' => 'card[state_id]',
                             'prompt' => '请输入开户银行卡所在的省份',
-                            'class' => 'text-right',
+                            'class' => 'text-right color-000',
                         ));
                         ?>
                     </div>
@@ -108,25 +114,78 @@ $this->show_footer = false;
                         echo $form->dropDownList($model, 'city_id', $model->loadOptionsCity(), array(
                             'name' => 'card[city_id]',
                             'prompt' => '请输入开户银行卡所在的城市',
-                            'class' => 'text-right pl10',
+                            'class' => 'text-right color-000',
                         ));
                         ?>
                     </div>
                 </div>
             </div>
-        </div>
+            </div>
+           <!-- 
+         -->
         <?php
         $this->endWidget();
         ?>
+        <div class="container">
+            <div class="text-left wrapper">
+                <form id="idCard-form" data-url-uploadfile="<?php echo $ajaxDoctorRealAuth; ?>" data-url-return="<?php echo $urlCardList; ?>">
+                    <input type="hidden" id="domain" value="http://drcert.file.mingyizhudao.com">
+                    <input type="hidden" id="uptoken_url" value="<?php echo $urlQiniuAjaxToken; ?>">
+                </form>
+            </div>
+        </div>
+        <div class="grid pt10 pl10 pr10 pb10 bg-white mt10">
+                <div class="col-1 ">
+                    <div >请上传一张银行卡的正面照</div>
+                   
+                </div>
+                <div class="col-0">
+                    <div class="body">
+                        <div class="">
+                            <div id="container1" class="uploadTab">
+                                <a class="btn btn-lg " id="pickfiles1" href="#">
+                                    <span>
+                                        <img src="" class="w70p h70">
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-12 mt10 hide">
+                            <table class="table table-striped table-hover text-left" style="display:none">
+                                <tbody id="fsUploadProgress1">
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        
         <div class="pad10 mt20">
             <!--  <button id="submitBtn"  class="btn btn-full btn-yellow3">下一步</button> -->
             <button id="submitBtn"  class="btn btn-full btn-yellow3"disabled="disabled">下一步</button>
         </div>
+
     </div>
 </article>
 <script>
     $(document).ready(function () {
-        document.addEventListener('input', function (e) {
+      var isFirst='<?php echo $isFirst;?>';
+      if(isFirst=='0'){
+         J.customConfirm('',
+                        '<div class="text-justify">尊敬的医生，因账户服务优化提升，烦请您再次填写正确的银行卡信息。谢谢您的谅解和支持。</div>',
+                        '<a id="close" class=" color-green">我知道了</a>',
+                        '',
+                        function () {
+                        },
+                        function () {
+                        });
+         $("#close").click(function(){
+                J.closePopup();
+             });
+
+      }
+
+       document.addEventListener('input', function (e) {
             checkInput();
         });
        $("select#card_state_id").change(function () {
