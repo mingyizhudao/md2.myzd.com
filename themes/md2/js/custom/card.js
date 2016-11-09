0e89353312222bdeba7d3ec19400ce60419b222d
jiangyan <18556506973@163.com>
2016-11-09 20:47:17 +0800

    p

--- themes/md2/js/custom/card.js
+++ themes/md2/js/custom/card.js
@@ -1,79 +1,11 @@
 $(function () {
-    var domForm = $('#card-form'),
-            submitBtn = $('#submitBtn');
-    submitBtn.click(function () {
-        // alert('a');
-        var cardTest = /^(\d{15,20})$/;
-        var cardNo = $('input[name="card[card_no]"]').val();
-       if (!cardTest.test(cardNo)) {
-            J.showToast('银行卡号不正确', '', '1500');
-            return;
-        }
-        var bool = validator.form();
-        if (bool) {
-            formAjaxSubmit();
-        }
-    });
-
-    var validator = domForm.validate({
-        rules: {
-            
-            'card[card_no]': {
-                required: true
-            },
-            'card[identification_card]': {
-                required: true
-            },
-            'card[state_id]': {
-                required: true
-            },
-            'card[city_id]': {
-                required: true
-            },
-            'card[bank]': {
-                required: true
-            },
-            
-            'card[is_default]': {
-                required: true
-            }
-        },
-        messages: {
-            
-            'card[card_no]': {
-                required: '请输入您的银行卡号'
-            },
-            'card[identification_card]': {
-                required: '请输入开户人身份证号'
-            },
-            'card[state_id]': {
-                required: '省份'
-            },
-            'card[city_id]': {
-                required: '城市'
-            },
-            'card[bank]': {
-                required: '开户银行'
-            },
-            
-            'card[is_default]': {
-                required: '设为默认'
-            }
-        },
-        errorElement: 'div',
-        errorPlacement: function (error, element) {
-//            element.parents('div.input').find('div.error').remove();
-//            element.parents('div.input').find('div.errorMessage').remove();
-//            error.appendTo(element.parents('.inputRow'));
-            element.removeClass('error');
-            element.addClass('error');
-        }
-    });
+    
 
     function formAjaxSubmit() {
         disabled(submitBtn);
+       domForm = $('#card-form');
         var formdata = domForm.serializeArray();
-        // console.log(formdata);
+        console.log('uu',formdata);
         var requestUrl = domForm.attr('data-action-url');
         var returnUrl = domForm.attr('data-return-url');
         var dataArray = structure_formdata('card', formdata);
@@ -84,9 +16,10 @@ $(function () {
             url: requestUrl,
             data: param,
             success: function (data) {
-                console.log('aa',data);
+                console.log('aaa',data);
                 if (data.status == 'ok') {
-                   location.href = returnUrl;
+                    
+                   // location.href = returnUrl;
                 } else {
                    enable( submitBtn);
                 }
@@ -212,7 +145,77 @@ $(function () {
                         success: function (data) {
                             console.log('bbb',data);
                             if (data.status == 'ok') {
-                                location.href = urlReturn;
+                                  var domForm = $('#card-form');
+            submitBtn = $('#submitBtn');
+   submitBtn.click(function () {
+        // alert('a');
+        var cardTest = /^(\d{15,20})$/;
+        var cardNo = $('input[name="card[card_no]"]').val();
+       if (!cardTest.test(cardNo)) {
+            J.showToast('银行卡号不正确', '', '1500');
+            return;
+        }
+        var bool = validator.form();
+        if (bool) {
+            formAjaxSubmit();
+        }
+       
+    });
+   var validator = domForm.validate({
+        rules: {
+            
+            'card[card_no]': {
+                required: true
+            },
+            'card[identification_card]': {
+                required: true
+            },
+            'card[state_id]': {
+                required: true
+            },
+            'card[city_id]': {
+                required: true
+            },
+            'card[bank]': {
+                required: true
+            },
+            
+            'card[is_default]': {
+                required: true
+            }
+        },
+        messages: {
+            
+            'card[card_no]': {
+                required: '请输入您的银行卡号'
+            },
+            'card[identification_card]': {
+                required: '请输入开户人身份证号'
+            },
+            'card[state_id]': {
+                required: '省份'
+            },
+            'card[city_id]': {
+                required: '城市'
+            },
+            'card[bank]': {
+                required: '开户银行'
+            },
+            
+            'card[is_default]': {
+                required: '设为默认'
+            }
+        },
+        errorElement: 'div',
+        errorPlacement: function (error, element) {
+//            element.parents('div.input').find('div.error').remove();
+//            element.parents('div.input').find('div.errorMessage').remove();
+//            error.appendTo(element.parents('.inputRow'));
+            element.removeClass('error');
+            element.addClass('error');
+        }
+    });
+                                // location.href = urlReturn;
                             }
                         }
                     });
